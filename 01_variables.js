9e127c3ebf53ad226a7eaac25a719cfd47e2ea53
Wowa3366-stack <matkovskyi36@ukr.net>
2026-02-03 20:38:31 +0200

    add js variables

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const myName = "vova";
+console.log(myName)
+
+const myAge = 13;
+console.log(myAge)
+
+console.log(typeof myName);
+console.log(typeof myAge);
+
+const productName = "cup";
+console.log(productName)
+
+let productPrice = 6;
+console.log(productPrice)
+
+productPrice = 10;
+console.log(productPrice)
