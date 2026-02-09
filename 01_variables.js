3ba37cbe3a121e8782b261c1b617e2472abd152c
Wowa3366-stack <matkovskyi36@ukr.net>
2026-02-09 20:16:41 +0200

    Update 01_variables.js

--- 01_variables.js
+++ 01_variables.js
@@ -1,17 +1,30 @@
-const myName = "vova";
-console.log(myName)
+// const myName = "vova";
+// console.log(myName)
 
-const myAge = 13;
-console.log(myAge)
+// const myAge = 13;
+// console.log(myAge)
 
-console.log(typeof myName);
-console.log(typeof myAge);
+// console.log(typeof myName);
+// console.log(typeof myAge);
 
-const productName = "cup";
-console.log(productName)
+// const productName = "cup";
+// console.log(productName)
 
-let productPrice = 6;
-console.log(productPrice)
+// let productPrice = 6;
+// console.log(productPrice)
 
-productPrice = 10;
-console.log(productPrice)
+// productPrice = 10;
+// console.log(productPrice)
+
+// const areYouStudent = true
+// console.log(areYouStudent)
+
+// 1.Створити повідомлення
+// 2.Вивести повідомлення користувачу
+// 3.Зберегти результат користувача
+// 4.Вивести у консоль
+
+const message = "Чи хоче ти продовжити підписку?";
+confirm(message);
+const renewSub = confirm(message);
+console.log(renewSub)
