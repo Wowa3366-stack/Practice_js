b87dda864f8778c49bb843c5df372953c0995f6b
Wowa3366-stack <matkovskyi36@ukr.net>
2026-06-16 18:51:20 +0300

    Update

--- DOM/script.js
+++ DOM/script.js
@@ -1,33 +1,54 @@
 const listEl = document.querySelector(".site-nav");
-console.log(listEl);
-const firstChild = listEl.firstElementChild;
-console.log(firstChild.textContent);
-firstChild.firstElementChild.style.color = "green";
 
-const ulListChildren = listEl.children;
-console.log(ulListChildren);
+const firstListChild = listEl.firstElementChild;
+firstListChild.style.color = "green";
+firstListChild.firstElementChild.style.color = "green";
 
-for (const child of ulListChildren) {
-  const link = child.firstElementChild;
-  console.log(link);
+const allChildrens = listEl.children;
 
-  console.log(child);
-  link.classList.add("nav-link");
+for (const child of allChildrens) {
+    child.firstElementChild.classList.add("nav-link");
 }
 
+const linksEl = document.querySelectorAll(".site-nav__item > a");
 
-const linksEl = document.querySelectorAll('.site-nav__item > a')
-console.log(linksEl);
-
-for (const link of linksEl){
-    console.log(link);
-    link.classList.add("new-link")
+for (const link of linksEl) {
+    link.classList.add("new-link");
 }
 
-const inputEl = document.querySelector('input')
-console.log(inputEl);
-const inputValue = inputEl.value
-console.log(inputValue);
-const buttonEl = document.querySelector('button[type = "button"]')
-console.log(buttonEl)
-buttonEl.textContent = inputValue
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector('button[type="button"]');
+
+buttonEl.textContent = inputEl.value;
+
+const newList = document.querySelector(".js_list");
+
+newList.innerHTML =
+    '<li class="js_item"><a href="./index.html"><span>hi</span></a></li>';
+
+newList.insertAdjacentHTML(
+    "afterbegin",
+    '<li class="js_item"><a href="./index.html"><span>nooo</span></a></li>'
+);
+
+const colorPickerContainer = document.querySelector(".js-color-picker");
+
+const colorPickerOptions = [
+    { label: "red", color: "#F44336" },
+    { label: "green", color: "#4CAF50" },
+    { label: "blue", color: "#2196F3" },
+    { label: "grey", color: "#607D8B" },
+    { label: "pink", color: "#E91E63" },
+    { label: "indigo", color: "#3F51B5" },
+];
+
+const markup = colorPickerOptions
+    .map(
+        option =>
+            `<button type="button" style="background-color:${option.color}">
+                label ${option.label}
+            </button>`
+    )
+    .join("");
+
+colorPickerContainer.innerHTML = markup;
---
+++ events/script.js
@@ -0,0 +1,44 @@
+const button = document.querySelector(".js-button")
+const onButtonClick = (event) =>{
+const button = event.target
+// console.log(button);
+button.style.backgroundColor = "green"
+}
+
+button.addEventListener("click", onButtonClick)
+
+const addStyleBtn = document.querySelector(".add-style-btn")
+const title = document.querySelector(".js-title")
+const onAddStyleBtnClick = () =>{
+title.classList.add("new-title") 
+}
+
+addStyleBtn.addEventListener("click", onAddStyleBtnClick)
+
+const form = document.querySelector(".form")
+const text = document.querySelector(".greeting")
+const onFormSubmit = (event) =>{
+    event.preventDefault()
+    console.log(event.target);
+     const form = event.target
+     console.log(form.elements);
+     const name = form.elements.name.value
+     const surname = form.elements.surname.value
+     const gender = form.elements.gender.value
+     const masagge = `Дякую ${name} ${surname} (${gender}) за реєстрацію`
+     text.textContent = masagge
+
+}
+
+form.addEventListener("submit", onFormSubmit)
+
+
+const masaggeInput = document.querySelector(".masaggeInput")
+
+const masaggeText = document.querySelector(".masaggeText")
+const onInputChange = (event) =>{
+const value = event.currentTarget.value
+masaggeText.textContent = value
+}
+
+masaggeInput.addEventListener("input", onInputChange)
